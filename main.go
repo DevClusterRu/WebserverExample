90e3f641d3d47522da1eeb2f6213543fa1688bbf
Den <weblifeon@gmail.com>
2022-07-16 01:16:37 +0300

    first

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", hnd)
+	//http.HandleFunc("/export", csvExport.ExportListener)
+	log.Println("Starting webserver...")
+	err := http.ListenAndServe(":9999", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func hnd(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "Hello!")
+}
